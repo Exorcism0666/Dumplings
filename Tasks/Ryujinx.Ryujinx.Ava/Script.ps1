cb04512afbc1258bc9c0cc86df89433578a6fa6a
‌ <46537034+Exorcism0666@users.noreply.github.com>
2024-03-15 21:08:45 +0100

    Fix

--- Tasks/Ryujinx.Ryujinx.Ava/Script.ps1
+++ Tasks/Ryujinx.Ryujinx.Ava/Script.ps1
@@ -21,33 +21,25 @@ $this.CurrentState.Installer += [ordered]@{
 # ReleaseTime
 $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
 
+if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = ($Object1.body | ConvertFrom-Markdown).Html | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
 # ReleaseNotesUrl
 $this.CurrentState.Locale += [ordered]@{
   Key   = 'ReleaseNotesUrl'
-  Value = $ReleaseNotesUrl = 'https://upx.github.io/upx-news.txt'
+  Value = $Object1.html_url
 }
 
 switch -Regex ($this.Check()) {
   'New|Changed|Updated' {
-    try {
-      $Object2 = Invoke-RestMethod -Uri $ReleaseNotesUrl
-
-      $ReleaseNotesObject = ($Object2 -split '(?m)(?=^Changes in [\d\.]+ \(.+?\):\n)').Where({ $_.Contains($this.CurrentState.Version) }, 'First')[0]
-      if ($ReleaseNotesObject) {
-        # ReleaseNotes (en-US)
-        $this.CurrentState.Locale += [ordered]@{
-          Locale = 'en-US'
-          Key    = 'ReleaseNotes'
-          Value  = $ReleaseNotesObject -creplace '^Changes in [\d\.]+ \(.+?\):\n' | Format-Text
-        }
-      } else {
-        $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
-      }
-    } catch {
-      $_ | Out-Host
-      $this.Log($_, 'Warning')
-    }
-
     $this.Write()
   }
   'Changed|Updated' {
