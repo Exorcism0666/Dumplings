a7e628c356b9dfdd0294a3255b511cb1435cc5d9
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2024-04-10 14:23:57 +0200

    Update Script.ps1

--- Tasks/TheBrowserCompany.Arc/Script.ps1
+++ Tasks/TheBrowserCompany.Arc/Script.ps1
@@ -5,7 +5,18 @@ $this.CurrentState.Version = $Object1.AppInstaller.Version
 
 # Installer
 $this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
   InstallerUrl = $Object1.AppInstaller.MainPackage.Uri
+}
+# Récupérer le lien
+$Uri = $Object1.AppInstaller.MainPackage.Uri
+
+# Remplacer "x64" par "arm64"
+$NewUri = $Uri.Replace("x64", "arm64")
+
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $NewUri
 }
 
 switch -Regex ($this.Check()) {
